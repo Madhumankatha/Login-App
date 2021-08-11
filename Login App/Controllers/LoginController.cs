78142409db6d8d2d95fe59ba709f857a03b12836
madhu <madhumankatha@gmail.com>
2021-08-10 18:00:37 -0700

    Form Authentication

--- Login App/Controllers/LoginController.cs
+++ Login App/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Login_App.Controllers
 {
@@ -24,6 +25,13 @@ namespace Login_App.Controllers
 
             if (result)
             {
+                user loggedUser = db.users.First(u => u.username.Equals(user.username) && u.password.Equals(user.password));
+                Session["username"] = loggedUser.username;
+                Session["userid"] = loggedUser.id;
+
+                FormsAuthentication.SetAuthCookie(loggedUser.username, false);
+
+
                 ViewBag.Message = "Login Successfull!!";
                 return RedirectToAction("Index", "Home");
             }
@@ -44,9 +52,40 @@ namespace Login_App.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(user user)
         {
+
             db.users.Add(user);
             db.SaveChanges();
             return RedirectToAction("Index","Login");
+        }
+
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session["username"] = null;
+            Session["userid"] = null;
+            Session.RemoveAll();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            try
+            {
+                // Verification.    
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    // Info.    
+                    return this.Redirect(returnUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info    
+                throw ex;
+            }
+            // Info.    
+            return this.RedirectToAction("Index", "Home");
         }
 
     }
